1195fe15f6c91026ddb0839893b033880fb68eb1
hikkywannafly <ggraygon@gmail.com>
2024-09-19 13:27:01 +0700

    up

--- lab2_iamrich/android/app/src/main/kotlin/dimaodin/iamrich/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package dimaodin.iamrich
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lap1_frist_app/android/app/src/main/kotlin/com/example/lap1_frist_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.lap1_frist_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
